b6eb013218e00d29d5118d473bed0d1cd2cfae37
Steffen Moelter <steffen.moelter@sonormed.de>
2017-01-28 21:58:21 +0100

    changed output of json

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,9 +3,9 @@ class ChatController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def apiai
-    render json: {fulfillment: {
+    render json: {
         source: "apiai-wikitest-webhook-sample",
         displayText: params.to_unsafe_h.to_json.to_s
-    }}
+    }
   end
 end
