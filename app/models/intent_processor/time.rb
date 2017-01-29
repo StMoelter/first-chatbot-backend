608df6fbd60cd94ff77e59573f5246910e91d745
Steffen Moelter <steffen.moelter@sonormed.de>
2017-01-29 20:29:01 +0100

    Time has to be ::Time :-)

--- app/models/intent_processor/time.rb
+++ app/models/intent_processor/time.rb
@@ -1,7 +1,7 @@
 module IntentProcessor
   class Time
     def self.process(_entities = nil)
-      I18n.l(Time.now.in_time_zone('Berlin'), format: :date_time_long)
+      I18n.l(::Time.now.in_time_zone('Berlin'), format: :date_time_long)
     end
   end
 end
